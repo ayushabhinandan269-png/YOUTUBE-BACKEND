1bc99d052c0ddb51761f5e537c235bc5c6e68218
Ayush Abhinandan <ayushabhinandan269@gmail.com>
2026-01-30 23:53:58 +0530

    fix: export default router in auth routes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
-
+import protect from "./middleware/authMiddleware.js";
 dotenv.config();
 connectDB();
 
@@ -14,11 +14,15 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-app.get("/", (req, res) => {
-  res.send("YouTube Clone API running");
+app.get("/api/protected", protect, (req, res) => {
+  res.json({ message: "Protected route working" });
 });
 
-const PORT = process.env.PORT || 8000;
+app.get("/", (req, res) => {
+  res.status(200).send("YouTube Clone API running");
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
